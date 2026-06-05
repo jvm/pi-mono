ec411806b669b88edfafc2a14253eb7c7a77dda2
jvm <mocitodev@pm.me>
2026-06-05 23:37:59 +0100

    Add install telemetry to pi-agentsmd
    - Create src/install-telemetry.ts following the separate module pattern
    - Wire reportInstallTelemetry() call at extension startup
    - Export from src/index.ts for consistency with other packages

--- packages/pi-agentsmd/extensions/index.ts
+++ packages/pi-agentsmd/extensions/index.ts
@@ -1,7 +1,10 @@
 import type { ExtensionAPI, ExtensionCommandContext } from "@earendil-works/pi-coding-agent";
 import { handleInitCommand } from "../src/init.js";
+import { reportInstallTelemetry } from "../src/install-telemetry.js";
 
 export default function piAgentsMd(pi: ExtensionAPI) {
+  reportInstallTelemetry();
+
   pi.registerCommand("init", {
     description: "create an AGENTS.md file with instructions for Pi",
     handler: async (args, ctx) => {
--- packages/pi-agentsmd/src/index.ts
+++ packages/pi-agentsmd/src/index.ts
@@ -1,2 +1,3 @@
 export { handleInitCommand } from "./init.js";
+export { reportInstallTelemetry } from "./install-telemetry.js";
 export { INIT_PROMPT } from "./prompt.js";
